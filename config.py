daafd722fd5d7832801fce3a2330d27d4f395abe
Shikhar <ptksuraj99@gmail.com>
2021-12-08 21:48:19 +0530

    feat: add db migrations

--- config.py
+++ config.py
@@ -5,6 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-should-have-been-a-secret'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + basedir + 'app.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
