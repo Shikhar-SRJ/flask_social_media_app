befae6be5ba2bbef956b45d0c69022f4e2fc9375
Rohit sahu <rohits8853@gmail.com>
2021-12-09 21:34:37 +0530

    add sign in sign up and  G auth

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,57 @@
+import os
+import pathlib
 from app import app
-
+from flask import render_template
+from flask import  request, make_response
+from app.models import  *
+# import hashlib
 
 @app.route('/')
 def index():
-    return 'hii'
+    return render_template('index.html')
+
+# login routes here
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/loginsuccess', methods=['POST'])
+def loginSucess():
+    if request.method == 'POST':
+        username = request.form.get('userName')
+        password = request.form.get('password')
+        #hashing the input and comparing the hash
+        # hashedPassword = hashlib.md5(bytes(str(password),encoding='utf-8'))
+        # hashedPassword = hashedPassword.hexdigest()
+        result = db.session.query(User).filter(User.username==username, User.password_hash==password)
+        for row in result:
+            if (len(row.username)!=0 and len(row.password_hash)!=0):
+                print(row.username)
+                print(row.password_hash)
+                # return render_template('profile.html')
+                return f"Hello {row.username}! <br/> <a href='/'><button>Logout</button></a>"
+    return render_template('login.html', error='could not verify')
+
+
+# register routes
+@app.route('/register')
+def register():
+    return render_template('register.html')
+@app.route('/registersuccess', methods=["POST"])
+def registerSuccess():
+    # app.logger.info('Info level log')
+    # app.logger.warning('Warning level log')
+    if request.method == "POST":
+        username = request.form.get('userName')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm_password')
+        if confirm_password == password:
+            #hashing the password before storing
+            # hashedPassword = hashlib.md5(bytes(str(password),encoding='utf-8'))
+            # hashedPassword = hashedPassword.hexdigest()
+            entry = User(username=username,email=email,password_hash=password)
+            db.session.add(entry)
+            db.session.commit()
+            print('register successfull')
+    return render_template('login.html')
--- config.py
+++ config.py
@@ -5,6 +5,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-should-have-been-a-secret'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + basedir + 'app.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db');
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    
+    
+    
+    
+    
+    # export FLASK_ENV=development
+    
     
