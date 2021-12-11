20fc92a16e3072f748515dc66b1a49c4c1e0248b
Shikhar <ptksuraj99@gmail.com>
2021-12-11 19:46:53 +0530

    refactor: change formattings

--- app/routes.py
+++ app/routes.py
@@ -90,14 +90,14 @@ def follow(username):
     if form.validate_on_submit():
         user = User.query.filter_by(username=username).first()
         if user is None:
-            flash('User {} not found.'.format(username))
+            flash(f'User {username} not found.')
             return redirect(url_for('index'))
         if user == current_user:
             flash('You cannot follow yourself!')
             return redirect(url_for('user', username=username))
         current_user.follow(user)
         db.session.commit()
-        flash('You are following {}!'.format(username))
+        flash(f'You are following {username}!')
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
@@ -110,14 +110,14 @@ def unfollow(username):
     if form.validate_on_submit():
         user = User.query.filter_by(username=username).first()
         if user is None:
-            flash('User {} not found.'.format(username))
+            flash(f'User {username} not found.')
             return redirect(url_for('index'))
         if user == current_user:
             flash('You cannot unfollow yourself!')
             return redirect(url_for('user', username=username))
         current_user.unfollow(user)
         db.session.commit()
-        flash('You are not following {}.'.format(username))
+        flash(f'You are not following {username}.')
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
